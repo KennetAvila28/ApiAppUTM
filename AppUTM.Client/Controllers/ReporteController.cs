19eb3a8c8daf4e35f7dc2df3d88cfe0021ab62ea
CarlosBe115 <72365048+CarlosBe115@users.noreply.github.com>
2021-07-25 22:52:51 -0500

    actualizacion reportes

---
+++ AppUTM.Client/Controllers/ReporteController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppUTM.Client.Controllers
+{
+    public class ReporteController : Controller
+    {
+        public IActionResult Reporte()
+        {
+            return View();
+        }
+    }
+}
